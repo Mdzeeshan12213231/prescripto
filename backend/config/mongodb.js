d73a3c0d9b6cfcb7bc72fbb1efd78e792bd6e967
unknown <zk00465@gmail.com>
2025-07-24 16:10:41 +0530

    Docs: Add correct MongoDB URI usage instructions

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
-const connectDB = async () => {
+// Make sure your .env contains:
+// MONGODB_URI=mongodb+srv://username:password@cluster.mongodb.net
+// Do NOT add /prescripto or ?w=majority/prescripto in the URI. The code below will append /prescripto automatically.
 
+const connectDB = async () => {
     mongoose.connection.on('connected', () => console.log("Database Connected"))
     await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
-
 }
 
 export default connectDB;
 
-// Do not use '@' symbol in your databse user's password else it will show an error.
+// Do not use '@' symbol in your database user's password else it will show an error.
